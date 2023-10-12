4643a885d59872affbb556910247cbe6bb651f73
dasom222g <dasom228@gmail.com>
2023-10-12 16:34:21 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div>hello World</div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
