a67656d56ad724ae7624c0d0857a76276d1d847d
dasom222g <dasom228@gmail.com>
2023-10-17 11:34:51 +0900

    Set router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
